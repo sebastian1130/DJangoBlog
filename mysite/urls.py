01e33bcffffab7c9af754d9575ebbbdbb6094047
sebastian1130 <sebastian_1130@outlook.com>
2018-11-08 08:53:02 -0500

    Adding posts and saving them from the page itself

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
 ]
