2e0a4ba773561879904d7314f3a35f6cf9bd9d53
sebastian1130 <sebastian_1130@outlook.com>
2018-11-08 09:00:04 -0500

    Added some security por adding and editing posts

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
+     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
