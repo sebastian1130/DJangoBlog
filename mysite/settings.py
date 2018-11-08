71b619aa343a1c9385f18b3eaa5a2c469c30c7e9
sebastian1130 <sebastian_1130@outlook.com>
2018-11-07 20:13:06 -0500

    Se solucionó una parte, ahora Bad Request

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6ee)!w1!76i3#cb@8^pvt35qmt3_pns)3nurzex@%(-03^zk*z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', ' .pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
